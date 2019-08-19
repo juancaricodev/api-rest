aedee38cbeab49b628f8512dd1e03c5b4200bfbc
Juan Camilo Rico <juancarico92@gmail.com>
2019-08-19 09:26:29 -0500

    GET implementation

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 
+const Product = require('./models/product')
+
 const app = express()
 const port = process.env.PORT || 3001
 
@@ -15,17 +17,41 @@ app.use(bodyParser.json())
 // })
 
 app.get('/api/product', (req, res) => {
-    res.send(200, {products: []})
+    Product.find({}, (err, products) => {
+        if (err) return res.status(500).send({message: `Error al realizar la petición: ${err}`})
+        if (!products) return res.status(404).send({message: `No existen productos`})
+
+        res.status(200).send({ products })
+    })
 })
 
 app.get('/api/product/:productId', (req, res) => {
-    
+    let productId = req.params.productId
+
+    Product.findById(productId, (err, product) => {
+        if (err) return res.status(500).send({message: `Error al realizar la petición: ${err}`})
+        if (!product) return res.status(404).send({message: `El producto no existe`})
+
+        res.status(200).send({ product })
+    })
 })
 
 app.post('/api/product', (req, res) => {
+    console.log(`POST /api/models/product`)
     console.log(req.body)
-    // res.send(200, {message: `El producto se ha recibido`})
-    res.status(404).send({message: `El producto no existe`})    
+
+    let product = new Product()
+    product.name = req.body.name
+    product.picture = req.body.picture
+    product.price = req.body.price
+    product.category = req.body.category
+    product.description = req.body.description
+
+    product.save((err, productStored) => {
+        if (err) res.status(500).send({message: `Error al guardar en al base de datos: ${err}`})
+
+        res.status(200).send({product: productStored})
+    })
 })
 
 app.put('/api/product/:productId', (req, res) => {
